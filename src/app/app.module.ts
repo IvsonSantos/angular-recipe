dc9d1a883c439d9112c9ecb4ad609e1334c93058
ivsonsantos <Belgique@2024>
2020-12-15 20:35:04 +0100

    recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { Recipes7recipeListComponent } from './recipes7recipe-list/recipes7recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    Recipes7recipeListComponent
   ],
   imports: [
     BrowserModule,
