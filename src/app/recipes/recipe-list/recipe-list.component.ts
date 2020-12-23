651be1b55be36d3bb9ecf4e70c4195e1ef2b8468
ivsonsantos <Belgique@2024>
2020-12-23 19:04:40 +0100

    Lista bonita e usavel

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Test', 'A test recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg?webp=true&quality=90&resize=500%2C454')
+  ];
 
   constructor() { }
 
